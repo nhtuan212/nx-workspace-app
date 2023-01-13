2b0d7dcd12c12e25efe5974e066af64aa13cdb0f
Nguyen Hoang Tuan <tuan.nguyen.hoang@vieon.vn>
2023-01-13 12:56:45 +0700

    #add project

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -18,8 +18,6 @@ export default function Home() {
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
-            Get started by editing&nbsp;
-            Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
           <div>
