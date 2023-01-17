d8f6cf504e1f62a1d5e427994d1e69d3ea92d749
Nguyen Hoang Tuan <tuan.nguyen.hoang@vieon.vn>
2023-01-17 17:31:32 +0700

    #add project

--- packages/frontend/src/components/Pages/Home/index.tsx
+++ packages/frontend/src/components/Pages/Home/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HomePage = () => {
     return (
-        <div className="d-block">
+        <div className="d-flex align-start lg:align-baseline">
             <p>This is Home Page</p>
         </div>
     );
