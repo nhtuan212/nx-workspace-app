76186fdfc0de7724f5e647c17436f8f7e9020c31
Nguyen Hoang Tuan <tuan.nguyen.hoang@vieon.vn>
2023-01-31 17:29:24 +0700

    #add project

--- packages/frontend/src/components/Pages/Home/index.tsx
+++ packages/frontend/src/components/Pages/Home/index.tsx
@@ -4,6 +4,10 @@ const HomePage = () => {
     return (
         <div className="d-flex align-start lg:align-baseline">
             <p>This is Home Page</p>
+            <div className="d-flex">
+                <div className="col-6 md:col-6 sm:col-8">Test col</div>
+                <div className="col-6 md:col-6 sm:col-8">Test col</div>
+            </div>
         </div>
     );
 };
