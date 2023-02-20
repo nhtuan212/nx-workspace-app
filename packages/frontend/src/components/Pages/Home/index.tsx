26180af245ae5243a4edaeaefff7b6c622c4168c
Hoang Tuan <nhtuan212@gmail.com>
2023-02-20 22:55:40 +0700

    #add project

--- packages/frontend/src/components/Pages/Home/index.tsx
+++ packages/frontend/src/components/Pages/Home/index.tsx
@@ -7,7 +7,7 @@ const HomePage = () => {
             <div className="d-flex">
                 <div className="col-6 md:col-6 sm:col-8">Test col</div>
                 <div className="col-6 md:col-6 sm:col-8">Test col</div>
-                <div className="mx-n0.25">Test col</div>
+                <div className="mx-n0.25 border border-top-0 rounded">Test col</div>
             </div>
         </div>
     );
