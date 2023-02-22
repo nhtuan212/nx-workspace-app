b54924f1edcb421b3d61065393e0db4221e61a57
Hoang Tuan <nhtuan212@gmail.com>
2023-02-22 21:20:54 +0700

    #add project

--- packages/frontend/src/components/Search/index.tsx
+++ packages/frontend/src/components/Search/index.tsx
@@ -2,15 +2,33 @@ import React, { useState } from 'react';
 import { ICON_KEY } from '@/constants';
 import Button from '../Button';
 import Input from '../Input';
+import SvgIcons from '../Icons/SvgIcons';
+import classNames from 'classnames';
+import styles from './searchStyles.module.scss';
 
 const index = () => {
+    const searchClassName = classNames(
+        styles['search-header'],
+        'search-head absolute top-0 w-100 d-flex justify-between align-center border rounded-md',
+    );
+    const inputSearchClassName = classNames(styles['input']);
+
     const handleOnClick = () => {
         console.log('onclick');
     };
     return (
-        <div className="search pointer">
+        <div className="search relative pointer w-100">
             <Button svgIconKind={ICON_KEY.SEARCH} onClick={handleOnClick} />
-            <Input className="absolute" text="input nè" />
+            <div className={searchClassName}>
+                <div className="col">
+                    <Input className={inputSearchClassName} />
+                </div>
+                <div className="p-0.5">
+                    <span className="icon">
+                        <SvgIcons kind={ICON_KEY.SEARCH} />
+                    </span>
+                </div>
+            </div>
         </div>
     );
 };
