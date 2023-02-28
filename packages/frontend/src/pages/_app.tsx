8b3effccc93b8d4192bac63be92ea5c07ab36baa
Nguyen Hoang Tuan <tuan.nguyen.hoang@vieon.vn>
2023-02-28 17:19:09 +0700

    #add project

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
         <Provider store={store}>
             <View {...pageProps}>
-                <Component />
+                <Component {...pageProps} />
             </View>
         </Provider>
     );
