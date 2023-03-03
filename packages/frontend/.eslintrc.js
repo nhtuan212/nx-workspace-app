88a3b92f8fe8cea4a6c235837733215b71bf990c
Nguyen Hoang Tuan <tuan.nguyen.hoang@vieon.vn>
2023-03-03 15:14:52 +0700

    #add project

--- packages/frontend/.eslintrc.js
+++ packages/frontend/.eslintrc.js
@@ -1,37 +1,40 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'es2021': true
-	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:@typescript-eslint/recommended'
-	],
-	'overrides': [
-	],
-	'parser': '@typescript-eslint/parser',
-	'parserOptions': {
-		'ecmaVersion': 'latest',
-		'sourceType': 'module'
-	},
-	'plugins': [
-		'react',
-		'@typescript-eslint'
-	],
-	'rules': {
-		'indent': 'off',
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    'overrides': [
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react',
+        '@typescript-eslint'
+    ],
+    'rules': {
+        'indent': [
+            'off',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
 		],
 		'@typescript-eslint/no-explicit-any': 'off',
 		'@typescript-eslint/ban-types': [
@@ -43,6 +46,7 @@ module.exports = {
 				}
 			}
 		],
+		'@typescript-eslint/no-unused-vars': 'error',
 		'react/no-unknown-property': [
 			2,
 			{
@@ -52,5 +56,5 @@ module.exports = {
 				]
 			}
 		]
-	}
+    }
 };
