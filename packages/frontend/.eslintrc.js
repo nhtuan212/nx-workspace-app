5c80e7a2ecf3f3fbd07823397fbbe7fa654fd9c7
Nguyen Hoang Tuan <tuan.nguyen.hoang@vieon.vn>
2023-03-07 10:50:55 +0700

    #add project

--- packages/frontend/.eslintrc.js
+++ packages/frontend/.eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
     'extends': [
         'eslint:recommended',
         'plugin:react/recommended',
-		'plugin:@next/next/recommended',
         'plugin:@typescript-eslint/recommended'
     ],
 	'settings': {
