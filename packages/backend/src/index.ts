38f8acf175609815d7ab50ad610937561a79dc22
Nguyen Hoang Tuan <tuan.nguyen.hoang@vieon.vn>
2023-03-29 13:33:35 +0700

    #add project

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -4,8 +4,6 @@ const port = 2000;
 
 app.get('/api', (req: any, res: any) => {
     res.send('Hello World!');
-    res.send('Hello World2!');
-    res.send('Hello World3!');
 });
 
 app.listen(port, () => {
