a941a458925c997f450d3712248e4f8578c7da56
Hoang Tuan <nhtuan212@gmail.com>
2023-08-26 09:01:56 +0700

    #add project

--- packages/frontend/.eslintrc.js
+++ packages/frontend/.eslintrc.js
@@ -1,67 +1,52 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    'extends': [
+    extends: [
         'eslint:recommended',
         'plugin:react/recommended',
-		'plugin:@next/next/recommended',
-        'plugin:@typescript-eslint/recommended'
+        'plugin:@next/next/recommended',
+        'plugin:@typescript-eslint/recommended',
     ],
-	'settings': {
-		'react': {
-			'version': 'detect'
-		}
-	},
-    'overrides': [
-    ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
+    settings: {
+        react: {
+            version: 'detect',
+        },
     },
-    'plugins': [
-        'react',
-        '@typescript-eslint'
-    ],
-	'rules': {
-        'indent': [
-            'off',
-            4
-        ],
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        // off|| 0 - warn || 1 - error || 2
+        indent: ['off', 4],
         // 'linebreak-style': [
         //     'error',
         //     'unix'
         // ],
-        'quotes': [
+        quotes: ['warn', 'single'],
+        semi: ['warn', 'always'],
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/ban-types': [
             'error',
-            'single'
+            {
+                extendDefaults: true,
+                types: {
+                    '{}': false,
+                },
+            },
         ],
-        'semi': [
-            'error',
-            'always'
-		],
-		'@typescript-eslint/no-explicit-any': 'off',
-		'@typescript-eslint/ban-types': [
-			'error',
-			{
-				'extendDefaults': true,
-				'types': {
-					'{}': false
-				}
-			}
-		],
-		'@typescript-eslint/no-unused-vars': 'error',
-		'react/no-unknown-property': [
-			2,
-			{
-				'ignore': [
-					'jsx',
-					'global',
-				]
-			}
-		]
+        '@typescript-eslint/no-unused-vars': 'error',
+        'react/no-unknown-property': [
+            2,
+            {
+                ignore: ['jsx', 'global'],
+            },
+        ],
     },
-	'ignorePatterns': ["**/*.css", "**/*.scss"],
+    ignorePatterns: ['**/*.css', '**/*.scss'],
 };
